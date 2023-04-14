414889c340a2eed1fbb78221089173b149d091fd
George Luis Costa Ribeiro <georgel.jc98@gmail.com>
2023-04-14 00:30:00 -0300

    Lab JPA parte 2

--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Fornecedor.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Fornecedor.java
@@ -1,10 +1,80 @@
 package br.inatel.labs.labjpa.entity;
 
 import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Fornecedor {
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	public Fornecedor() {
+		super();
+	}
+
+	public Fornecedor(String razaoSocial) {
+		super();
+		this.razaoSocial = razaoSocial;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fornecedor other = (Fornecedor) obj;
+		return id == other.id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public List<Produto> getListaProduto() {
+		return ListaProduto;
+	}
+
+	public void setListaProduto(List<Produto> listaProduto) {
+		ListaProduto = listaProduto;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	private List<Produto> Produto;	
+
+	@Override
+	public String toString() {
+		return "Fornecedor [id=" + id + ", razaoSocial=" + razaoSocial + "]";
+	}
+
+	@ManyToMany // Dominante
+	private List<Produto> ListaProduto;
+
+	@NotNull
+	@Size(min = 2, max = 100)
 	private String razaoSocial;
 }
--- workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompraItem.java
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompraItem.java
@@ -3,14 +3,114 @@ package br.inatel.labs.labjpa.entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
 public class NotaCompraItem {
 
+	@Override
+	public String toString() {
+		return "NotaCompraItem [id=" + id + ", valorCompraProduto=" + valorCompraProduto + ", quantidade=" + quantidade
+				+ "]";
+	}
+
+	public NotaCompraItem() {
+		super();
+	}
+
+	public NotaCompraItem(NotaCompra notacompra, Produto produto, BigDecimal valorCompraProduto, Integer quantidade) {
+		super();
+		this.notacompra = notacompra;
+		this.produto = produto;
+		this.valorCompraProduto = valorCompraProduto;
+		this.quantidade = quantidade;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotaCompraItem other = (NotaCompraItem) obj;
+		return id == other.id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public NotaCompra getNotacompra() {
+		return notacompra;
+	}
+
+	public void setNotacompra(NotaCompra notacompra) {
+		this.notacompra = notacompra;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public BigDecimal getValorCompraProduto() {
+		return valorCompraProduto;
+	}
+
+	public void setValorCompraProduto(BigDecimal valorCompraProduto) {
+		this.valorCompraProduto = valorCompraProduto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@ManyToOne
 	private NotaCompra notacompra;
+
+	@ManyToOne
+	private Produto produto;
+
+	@NotNull
+	@Positive
 	private BigDecimal valorCompraProduto;
+
+	@NotNull
+	@Positive
 	private Integer quantidade = 1;
-	
+
 	public BigDecimal getCalculoTotalItem() {
 		return valorCompraProduto.multiply(BigDecimal.valueOf(quantidade));
 	}
